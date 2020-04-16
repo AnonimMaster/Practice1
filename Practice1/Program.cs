504ffd41c6b7fd14641c3b7a71cedc6575e3a66d
SuccubHunter <kirilloriginallolo@gmail.com>
2020-04-16 22:36:15 +0500

    Final Version

--- Practice1/Program.cs
+++ Practice1/Program.cs
@@ -6,11 +6,46 @@ using System.Threading.Tasks;
 
 namespace Practice1
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Введите размер массива");
+			int SizeArray = Convert.ToInt32(Console.ReadLine());
+			int[] numbers = new int[SizeArray];
+			InputArray(numbers);
+			OutInputArray(numbers);
+			MeanValueAndAmount(numbers, SizeArray);
+		}
 
-        }
-    }
+		static void InputArray(int[] numbers)
+		{
+			Random rand = new Random();
+			for(int i=0; i< numbers.Length; i++)
+			{
+				numbers[i] = rand.Next(100);
+			}
+		}
+
+		static void MeanValueAndAmount(int[] numbers, int SizeArray)
+		{
+			int _MeanValue=0;
+			int amount=0;
+			foreach(int number in numbers)
+			{
+				amount += number;
+			}
+			_MeanValue = amount / SizeArray;
+			Console.WriteLine("Среднее значение: {0}",_MeanValue);
+			Console.WriteLine("Сумма: {0}", amount);
+		}
+
+		static void OutInputArray(int[] numbers)
+		{
+			foreach (int number in numbers)
+			{
+				Console.WriteLine(number);
+			}
+		}
+	}
 }
